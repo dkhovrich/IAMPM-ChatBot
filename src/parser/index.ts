5bf0cd885d96ce64380b329aa748377bcb92f6f5
Dmitriy Khovrich <dkhovrich@lohika.com>
2018-07-18 12:47:48 +0300

    parser console.log added

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,11 +1,13 @@
 import { argv } from 'yargs';
 import GlossaryParser from './glossaryParser';
 import GlossaryService from '../services/glossaryService';
+import { IGlossaryDto } from '../models/glossaryDto';
 
 (async () => {
     try {
         const parser = new GlossaryParser(argv.path);
-        const glossaryItems = parser.execute();
+        const glossaryItems: IGlossaryDto[] = parser.execute();
+        console.log(`Parsed ${glossaryItems.length} glossary items`);
 
         await GlossaryService.createMany(glossaryItems);
         console.log('Glossary items successfully parsed');

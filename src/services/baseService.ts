a3d8a8e054f2410e66a79039670710f1ec9b3fd8
Dmitriy Khovrich <dkhovrich@lohika.com>
2018-07-18 17:02:02 +0300

    Feature/heroku prepare (#10)
    * Get rid of config.js. (#9)
    
    Use .env file for app configuration.
    
    * Project configuration for heroku deploy

--- src/services/baseService.ts
+++ src/services/baseService.ts
@@ -1,13 +1,11 @@
 import mongoose from 'mongoose';
-import config from '../config';
-import ValidationError from '../errors/validationError';
 
 type Action = () => Promise<any>;
 
 abstract class BaseService {
     protected async handleConnection(action: Action): Promise<any> {
         try {
-            await mongoose.connect(config.databaseEndPoint);
+            await mongoose.connect(process.env.DB_HOST);
             return await action();
         } catch (err) {
             throw err;

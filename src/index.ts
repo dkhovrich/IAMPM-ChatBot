a2a7f3b6d0e05cdc68c7c96db8138028b2ec48fc
Dmitriy Khovrich <dkhovrich@lohika.com>
2018-07-20 11:13:53 +0300

    Base url for admin api changed.

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/auth', auth);
-app.use('/users', passport.authenticate('jwt', authOptions), users);
-app.use('/glossaries', passport.authenticate('jwt', authOptions), glossaries);
+app.use('/admin/users', passport.authenticate('jwt', authOptions), users);
+app.use('/admin/glossaries', passport.authenticate('jwt', authOptions), glossaries);
 app.use('/chatbot/glossaries', chatbotGlossaries);
 
 app.use(errorHandler());

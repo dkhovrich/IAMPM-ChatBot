45ce2672112f31c4a74eabdf2ba16ab04772f858
Dmitriy Khovrich <khovrych.d@gmail.com>
2018-07-24 19:42:07 +0300

    Develop (#22)
    * Feature/jsonschema (#13)
    
    * jsonSchemaMiddleware for auth and user routers.
    
    * nodemon.json added
    
    * jsonSchemaMiddleware for glossary.
    
    * _.isString()
    
    * Return glossary array if there is more than one match. (#14)
    
    * Check if admin exists via .env file on start (#16)
    
    * Feature/glossary db model refactoring (#17)
    
    * Glossary model changed.
    
    * text index created
    
    * fix
    
    * Check default admin user on app start
    
    * Glossary search support (#19)
    
    * Feature/pagination (#21)
    
    * Pagination.
    Return glossaries with pagination.
    
    * pagination fix
    
    * default page size changed to 10

---
+++ src/middleware/paginationMiddleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+
+const DEFAULT_PAGE_SIZE = 10;
+const PAGE_NUMBER_PROPERTY = 'pageNumber';
+const PAGE_SIZE_PROPERTY = 'pageSize';
+
+export default function (pageSize: number = DEFAULT_PAGE_SIZE) {
+    return function (req: Request, res: Response, next: NextFunction) {
+        validateProperty(req.query, PAGE_NUMBER_PROPERTY, 1);
+        validateProperty(req.query, PAGE_SIZE_PROPERTY, pageSize);
+        next();
+    };
+}
+
+function validateProperty(object: any, propertyName: string, defaultValue: number): void {
+    if (!object.hasOwnProperty(propertyName)) {
+        object[propertyName] = defaultValue;
+        return;
+    }
+
+    object[propertyName] = Number(object[propertyName]);
+    if (object[propertyName] === 0) {
+        object[propertyName] = defaultValue;
+    }
+}
